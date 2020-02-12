22672956d48861ee4e2666059f7090421637fdb5
Felix Haus <felixhaus@gmail.com>
2020-02-12 20:15:06 +0100

    Initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const WriteFilePlugin = require("write-file-webpack-plugin");
+
+const OUTPUT_DIR = "../dist";
+const PROJECT_NAME = "storybook-stencil-example";
+
+module.exports = {
+  stories: ["../src/**/*.stories.js"],
+  addons: ["@storybook/addon-notes/register"],
+  async webpackFinal(config) {
+    config.entry.push(path.join(__dirname, OUTPUT_DIR, `${PROJECT_NAME}.js`));
+    fs.readdirSync(
+      path.join(__dirname, OUTPUT_DIR, "collection/components")
+    ).map(file => {
+      jsFilePath = path.join(
+        __dirname,
+        OUTPUT_DIR,
+        `collection/components/${file}/${file}.js`
+      );
+      try {
+        if (fs.existsSync(jsFilePath)) {
+          config.entry.push(jsFilePath);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+
+      cssFilePath = path.join(
+        __dirname,
+        OUTPUT_DIR,
+        `collection/components/${file}/${file}.css`
+      );
+      try {
+        if (fs.existsSync(cssFilePath)) {
+          config.entry.push(cssFilePath);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    });
+
+    config.plugins.push(
+      new CopyPlugin([
+        {
+          from: "**/*",
+          to: "./",
+          context: "dist"
+        }
+      ])
+    );
+
+    config.plugins.push(new WriteFilePlugin());
+
+    return config;
+  }
+};
---
+++ src/components/my-component/my-component.stories.js
@@ -0,0 +1,14 @@
+import { storiesOf } from "@storybook/html";
+
+import readme from "./readme.md";
+
+storiesOf("My Component", module).add(
+  "Default",
+  () =>
+    '<my-component first="Millie" middle="Bobby" last="Brown"></my-component>',
+  {
+    notes: {
+      markdown: readme
+    },
+  }
+);
